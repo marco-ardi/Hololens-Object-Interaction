8ff0affc9786b4e46bf13c5dcf475da4ddc883bf
Marco Ardizzone <marco.ardizzone99@gmail.com>
2021-10-11 16:57:11 +0200

    fix tcp socket

--- logstash/serverPy.py
+++ logstash/serverPy.py
@@ -2,7 +2,12 @@ import socket
 import cv2
 import io
 import csv
+import json
+import base64
 import numpy as np
+from ftfy import fix_text
+import json
+
 
 
 HOST = '172.20.10.3' # Host address
@@ -13,10 +18,21 @@ def recv_by_step(conn, size):
     mul = size // 8192
     reminder = size % 8192
     for i in range(mul):
-        immagine+=conn.recv(8192)
-    immagine+= conn.recv(reminder)
+        immagine+=conn.recv(8192, socket.MSG_WAITALL)
+    immagine+= conn.recv(reminder, socket.MSG_WAITALL)
+    print("i received:" + str(immagine))
     return immagine
 
+
+def recv_json(conn, size):
+    #json_data = recv_by_step(conn, size).decode("utf-8")
+    json_data = conn.recv(size, socket.MSG_WAITALL).decode("utf-8")
+    # text = some text source with a potential unicode problem
+    fixed_text = fix_text(json_data)
+    print(fixed_text)
+    parsed_data = json.loads(fixed_text)
+    #parsed_data = json.loads(json_data)
+    return parsed_data["msg"], base64.b64decode(parsed_data["img"])
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("listening on:" + HOST +":"+ str(PORT))
@@ -30,32 +46,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print('Connected by', addr)
             while True:
-                try:
-                    str_size = conn.recv(8).decode('utf-8') #TODO In C# usare str(len(string)).padRight(8,"0").encode()
-                    print("str_size = " + str_size)
+                json_size = conn.recv(8).decode('utf-8')
+                print("json_size=" + json_size)
 
-                    stringdata = conn.recv(int(str_size)).decode("utf-8")
-                    print("String: ", stringdata)
-                except Exception as e:
-                    print(e)
-                    break
-                if not stringdata: continue  
+                stringdata, img =recv_json(conn, int(json_size))
+                #try:
+                    #str_size = conn.recv(8).decode('utf-8') #TODO In C# usare str(len(string)).padRight(8,"0").encode()
+                    #print("str_size = " + str_size)
+
+                    #stringdata = conn.recv(int(str_size)).decode("utf-8")
+                    #print("String: ", stringdata)
+                #except Exception as e:
+                    #print(e)
+                    #break
+                #if not stringdata: continue  
 
                 received_data = stringdata.split(' ')
                 wr.writerow(received_data)
 
-                dim = conn.recv(8)
-                print("dim=")
-                print(dim)
-                dim = dim.decode("utf-8")
-                print("dim = "+dim)
-                img = recv_by_step(conn, int(dim))
+                #dim = conn.recv(8)
+                #print("dim=")
+                #print(dim)
+                #dim = dim.decode("utf-8")
+                #print("dim = "+dim)
+                #img = recv_by_step(conn, int(dim))
                 #img = conn.recv(int(dim))
                 print(img[:-10])
                 # img = conn.recv(int(dim)) #worst case scenario for a 1280x720 jpeg image is 160000 bytes
                 #print(img)
 
-                #if not img: continue         #it is 2764800 bytes for a 1280x720 png image
+                if not img: continue         #it is 2764800 bytes for a 1280x720 png image
                 try:
                     file_bytes = np.asarray(bytearray(io.BytesIO(img).read()), dtype=np.uint8)    
                     imgToShow = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
